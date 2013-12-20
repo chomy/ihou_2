ec60c6b28e89613c361e139cee19dc0579d7437a
Keisuke Nakao <chome@argv.org>
2013-12-20 22:42:14 +0900

    add comment and change variable name

--- sim.R
+++ sim.R
@@ -1,3 +1,4 @@
+# Low Pass Filter (LPF)
 lpf <- function(signal, time, cutoff, smplfreq){
 	offset <- min(signal)
 	if(offset>0){
@@ -9,17 +10,26 @@ lpf <- function(signal, time, cutoff, smplfreq){
 	abs(fft(spec, inverse=TRUE))/length(spec)+offset 
 }
 
-N <- 10000
-t <- 1:N/N
-C = 1
-A = 0.7
-sig <- (A*sin(2*pi*3*t) + C) * sin(2*pi*30*t)
+N <- 10000 # Sampling Frequency
+t <- 1:N/N # Time
+C = 1			 # Carrier Amplitude
+fc= 30     # Carrier Frequency
+A = 0.7    # Signal Amplitude
+fs= 3			 # Signal Frequency
+fl= 20     # Local Osc Freq
 
-Q = sig * cos(2*pi*32*t)
-I = sig * sin(2*pi*32*t)
+# AM Modulation
+Vam <- (A*sin(2*pi*fs*t) + C) * sin(2*pi*fc*t)
 
-cutoff = 980 
+Q = Vam * cos(2*pi*fl*t)
+I = Vam * sin(2*pi*fl*t)
+
+cutoff = 15 # Cutoff Frequency of LPF
 Q2 <- lpf(Q, t, cutoff, N)
 I2 <- lpf(I, t, cutoff, N)
-result = sqrt(Q2*Q2 + I2*I2)
-plot(t,result, type='l')
+
+# Demodulated Signal
+Vdm = sqrt(Q2*Q2 + I2*I2)
+
+# Outout Signal
+Vout = lpf(Vdm, t, 10, N)
